da0b8e481510823f14eb8da5063b64f7a2bd0ffd
madorTill <mador.till@gmail.com>
2024-07-01 14:11:31 +0300

    flex direction

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="home-page">
     <h1 class="title-text">water reminder</h1>
-    <p>בבקשה תעבוד</p>
+    <p class="title-text" >בבקשה תעבוד</p>
   </div>
 </template>
 
@@ -20,8 +20,11 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+  margin: 5%;
+  flex-direction: column;
 }
 .title-text {
+  margin: 2%;
   color: rgb(8, 51, 136);
 }
 </style>
